f1bcac23978edce8f76def1da9fe450b9c4ca4f0
Rick Lamoreaux <rlamorea@gmail.com>
2024-07-07 13:48:25 -0700

    fixes #77 - file command unit tests

--- test/mockMachine.js
+++ test/mockMachine.js
@@ -7,6 +7,7 @@ global.window = {
 }
 
 import { default as MachineX } from '../scripts/machine/machine.js'
+import FileSystem from '../scripts/machine/filesystem.js'
 
 import CharGridScreen from '../scripts/machine/screens/charGridScreen.js'
 
@@ -23,6 +24,55 @@ class mockClassList {
         delete this.classes[arg]
       }
     }
+  }
+}
+
+class MockFileSystem extends FileSystem {
+  constructor(machine, fsContents) {
+    super(machine)
+
+    this.fsContents = fsContents
+    this.currentDir = '/'
+  }
+  pathKey(filename) {
+    let pathKey = (filename === null) ? this.currentDir : (filename.startsWith(this.currentDir) ? '' : this.currentDir)
+    pathKey += (filename === null) ? '' : filename
+    return pathKey
+  }
+  async getCatalog(path, prefix, suffix) {
+    let pathKey = (path === undefined) ? this.currentDir : path
+    if (prefix) { pathKey += '+'+prefix }
+    if (suffix) { pathKey += '-'+suffix }
+    return { path: pathKey, files: this.fsContents[pathKey] }
+  }
+  async saveFile(fileContents, filename) {
+    const filepath = this.pathKey(filename)
+    this.fsContents[filepath] = fileContents
+    return { success: true, filepath }
+  }
+  async loadFile(filename) {
+    const filepath = this.pathKey(filename)
+    return { success: true, filepath, fileContents: this.fsContents[filepath] }
+  }
+  async setCurrentDirectory(path) {
+    this.currentDir = path
+    if (!this.currentDir.endsWith('/')) { this.currentDir += '/' }
+    return { created: true, path: this.currentDir }
+  }
+  async scratchFile(filename) {
+    delete this.fsContents[this.pathKey(filename)]
+    return true
+  }
+  async copyFile(filename, newfile) {
+    this.fsContents[this.pathKey(newfile)] = this.fsContents[this.pathKey(filename)]
+    return true
+  }
+  async renameFile(filename, newfile) {
+    const fkey = this.pathKey(filename)
+    const contents = this.fsContents[fkey]
+    delete this.fsContents[fkey]
+    this.fsContents[this.pathKey(newfile)] = contents
+    return true
   }
 }
 
@@ -49,6 +99,8 @@ export default class Machine extends MachineX {
   constructor(options = {}) {
     super({ ...options, noscreens: true })
 
+    this.fileSystem = new MockFileSystem(this, options.files)
+
     if (options.addScreen) {
       this.screenCells = []
 
@@ -68,7 +120,9 @@ export default class Machine extends MachineX {
       this.screens = {
         'LIVE': new CharGridScreen('fixed-test', this.div, this, {refCell}),
         'RUN': new CharGridScreen('run-test', this.div, this, {refCell}),
+        'EDIT': { resetEditor: () => { } }
       }
+      for (const s in this.screens) { const sc = this.screens[s]; sc.displayMessage = (m) => { sc.displayString(m) } }
       this.currentScreen = this.screens['LIVE']
       this.screen = this.screens['LIVE']
     }
